ae38fccd3659ee04a7d18f25088728b0bee5ee5c
Akhilesh Gowda K S <akhileshgowdaksag05@gmail.com>
2026-09-18 20:47:26 +0530

    Prepare app for Render deployment

--- src/main/java/com/ksrtc/bustimetable/repository/StopRepository.java
+++ src/main/java/com/ksrtc/bustimetable/repository/StopRepository.java
@@ -1,6 +1,7 @@
 package com.ksrtc.bustimetable.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +10,7 @@ import com.ksrtc.bustimetable.entity.Stop;
 public interface StopRepository extends JpaRepository<Stop, Long> {
 
     List<Stop> findByNormalizedName(String normalizedName);
+
+    Optional<Stop> findFirstByNormalizedName(String normalizedName);
 }
+
